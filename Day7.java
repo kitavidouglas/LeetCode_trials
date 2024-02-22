12aadd70e69100e9fb3f70444354a5d82bf5b35b
kitavidouglas <keymoneykitavi@gmail.com>
2024-02-23 00:01:51 +0300

    commit

---
+++ Day7.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Day7 {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));       // Output: true
+        System.out.println(isValid("()[]{}"));   // Output: true
+        System.out.println(isValid("(]"));       // Output: false
+        System.out.println(isValid("([)]"));     // Output: false
+        System.out.println(isValid("{[]}"));     // Output: true
+    }
+}
